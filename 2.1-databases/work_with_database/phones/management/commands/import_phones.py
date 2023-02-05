8065c62d4dfdecfe312dcce655ed5106b45c67a6
Matthewous <moguch98@mail.ru>
2023-02-05 21:17:43 +0300

    Прописан загрузчик

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -2,6 +2,7 @@ import csv
 
 from django.core.management.base import BaseCommand
 from phones.models import Phone
+from django.http import HttpResponse
 
 
 class Command(BaseCommand):
@@ -12,6 +13,20 @@ class Command(BaseCommand):
         with open('phones.csv', 'r') as file:
             phones = list(csv.DictReader(file, delimiter=';'))
 
+        string_num = 0
         for phone in phones:
-            # TODO: Добавьте сохранение модели
-            pass
+            if string_num == 0:
+                string_num += 1
+            else:
+                data = Phone(
+                    id = phone[0],
+                    name=phone[1],
+                    price=phone[2],
+                    image= phone[3],
+                    image = phone[4],
+                    release_date = phone[5],
+                    lte_exists = phone[6]
+                )
+                data.save()
+            return HttpResponse ('Данные импортированы')
+                
