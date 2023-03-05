2295ec7e7c4f570d8de6b3bdbf9dc952a5920100
Matthewous <moguch98@mail.ru>
2023-03-05 16:08:22 +0300

    show_product without context

--- databases/work_with_database/phones/views.py
+++ databases/work_with_database/phones/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from .models import Phone
 
 
 def index(request):
@@ -7,11 +8,14 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
-    return render(request, template, context)
+    data = Phone.objects.all()
+    return render(request, template, context={'phones':data})
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
-    return render(request, template, context)
+    data = Phone.objects.filter(slug=slug)
+    phone = {
+        phone.name:Phone.name(data),
+    }
+    return render(request, template, context={'phone':phone})
